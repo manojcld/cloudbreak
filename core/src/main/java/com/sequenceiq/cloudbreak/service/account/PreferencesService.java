67c7e9638cced8553798ce9ea26b012283ae0c5e
doktoric <rdoktorics@cloudera.com>
2022-02-09 17:53:11 +0100

    CB-15883 Ability to hide every provider and show only aws gov

--- core/src/main/java/com/sequenceiq/cloudbreak/service/account/PreferencesService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/account/PreferencesService.java
@@ -31,6 +31,9 @@ public class PreferencesService {
     @Value("${cb.enabledplatforms:}")
     private String enabledPlatforms;
 
+    @Value("${cb.enabledgovplatforms:}")
+    private String enabledGovPlatforms;
+
     @Inject
     private List<CloudConstant> cloudConstants;
 
@@ -57,6 +60,14 @@ public class PreferencesService {
         return platforms;
     }
 
+    public Set<String> enabledGovPlatforms() {
+        Set<String> platforms;
+        platforms = enabledGovPlatforms.isEmpty()
+                ? Sets.newHashSet()
+                : Sets.newHashSet(enabledGovPlatforms.split(","));
+        return platforms;
+    }
+
     public Map<String, Boolean> platformEnablement() {
         Map<String, Boolean> result = new HashMap<>();
         if (StringUtils.isEmpty(enabledPlatforms)) {
@@ -65,6 +76,25 @@ public class PreferencesService {
             }
         } else {
             for (String platform : enabledPlatforms()) {
+                result.put(platform, true);
+            }
+            for (CloudConstant cloudConstant : cloudConstants) {
+                if (!result.keySet().contains(cloudConstant.platform().value())) {
+                    result.put(cloudConstant.platform().value(), false);
+                }
+            }
+        }
+        return result;
+    }
+
+    public Map<String, Boolean> govPlatformEnablement() {
+        Map<String, Boolean> result = new HashMap<>();
+        if (StringUtils.isEmpty(enabledGovPlatforms)) {
+            for (CloudConstant cloudConstant : cloudConstants) {
+                result.put(cloudConstant.platform().value(), true);
+            }
+        } else {
+            for (String platform : enabledGovPlatforms()) {
                 result.put(platform, true);
             }
             for (CloudConstant cloudConstant : cloudConstants) {
